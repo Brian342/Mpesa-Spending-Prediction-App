7dfc6030b4146889a5f36e1239dd5cfd18427e74
Brian kimanzi <migelbrian3@gmail.com>
2025-04-27 17:47:49 +0300

    new changes

---
+++ static/script.js
@@ -0,0 +1,21 @@
+document.getElementById("predictBtn").addEventListener("click", function() {
+    const transactionAmount = document.getElementById("transactionAmount").value;
+
+    if (!transactionAmount) {
+        alert("Please enter a transaction amount!");
+        return;
+    }
+
+    fetch(`http://127.0.0.1:5000/prediction/${transactionAmount}`)
+        .then(response => response.json())
+        .then(data => {
+            const resultContainer = document.getElementById("result");
+            resultContainer.style.display = "block";
+            resultContainer.innerHTML = `<h3>Predicted Spending: ${data.prediction}</h3>`;
+        })
+        .catch(error => {
+            console.error("Error:", error);
+            alert("An error occurred. Please try again later.");
+        });
+});
+
