24d9b8b038371f93d1c0823029b437acb3283cc1
Brian kimanzi <migelbrian3@gmail.com>
2025-05-05 14:42:22 +0300

    new changes

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_restful import Resource, Api
 import pickle
 import pandas as pd
 from flask_cors import CORS
-from sklearn.preprocessing import LabelEncoder, StandardScaler
+# from sklearn.preprocessing import LabelEncoder, StandardScaler
 from util import encode_categorical_columns, encode_categorical_columns_training_encoder
 
 model_path = "Models/Mpesa_XGBRegressor.pkl"
